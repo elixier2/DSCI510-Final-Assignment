d3d0cffac68e2e3076155b7bb8f8ad498fef197a
elixier2 <166572779+elixier2@users.noreply.github.com>
2024-05-03 12:07:27 -0700

    Update 2_Learning.py

--- pages/2_Learning.py
+++ pages/2_Learning.py
@@ -1,8 +1,24 @@
 import streamlit as st
 
 
-st.title("QnA")
+st.title("Questions")
 
-st.subheader("Q1")
+st.subheader("Q1- What did you set out to study?  (i.e. what was the point of your project?  This should be close to your Milestone 1 assignment, but if you switched gears or changed things, note it here.")
 
-st.write("responsde")
+st.write("response")
+
+st.subheader("Q2- What did you Discover/what were your conclusions (i.e. what were your findings?  Were your original assumptions confirmed, etc.?")
+
+st.write("response")
+
+st.subheader("Q3- What difficulties did you have in completing the project?")
+
+st.write("response")
+
+st.subheader("Q4- What skills did you wish you had while you were doing the project?")
+
+st.write("response")
+
+st.subheader("Q5- What would you do “next” to expand or augment the project?")
+
+
